3eb5396b841aae4f175c3110403a6438e8a44d30
Jisus <aventurasdejesu@gmail.com>
2024-02-28 20:18:04 -0500

    primer commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let diaSemana = 1
+
+if (diaSemana => 1 && diaSemana <= 7) {
+}
+if (diaSemana === 1) {
+    console.log("Tu dia es el Lunes")
+}
+else if (diaSemana === 2) {
+    console.log("Tu dia es el Martes")
+}
+else if (diaSemana === 3) {
+    console.log("Tu dia es el Miercoles")
+}
+else if (diaSemana === 4) {
+    console.log("Tu dia es el Jueves")
+}
+else if (diaSemana === 5) {
+    console.log("Tu dia es el Viernes")
+}
+else if (diaSemana === 6) {
+    console.log("Tu dia es el Sabado")
+}
+else if (diaSemana === 7) {
+    console.log("Tu dia es el Domingo")
+}
+else {
+    console.log("Dame un dia de la semana valido ")
+}
---
+++ index2.js
@@ -0,0 +1,20 @@
+let n1 = 4;
+let n2 = 2;
+let operacion = "+"
+
+if (operacion === "+") {
+    let resultado = n1 + n2;
+    console.log(resultado)
+}
+else if (operacion === "-") {
+    let resultado = n1 - n2
+    console.log(resultado)
+}
+else if (operacion === "*") {
+    let resultado = n1 * n2
+    console.log(resultado)
+}
+else if (operacion === "/") {
+    let resultado = n1 / n2
+    console.log(resultado)
+}
